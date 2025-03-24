fd65e03b5d95dd825135aa90ec963fa570b7f155
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-24 13:01:58 +0600

    using react form

--- src/Components/Pages/Addproduct/ADDproduct.jsx
+++ src/Components/Pages/Addproduct/ADDproduct.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { useForm } from "react-hook-form"
 
 const ADDproduct = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm()
+      const onSubmit = (data) => console.log(data)
+    
     return (
-        <div className="max-w-md mx-auto bg-white rounded-lg p-6 ">
+        <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg p-6 ">
             <h2 className='text-xl font-semibold text-black mb-4'>Add new contact</h2>
-            <form action="">
+            <form action="" onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
                 <div>
                     <label className='text-sm font medium block text-gray-600'>product Name</label>
-                    <input type="text" name='name' id='name' placeholder='product 1' className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
+                    <input  {...register("name", { required: true })} type="text" name='name' id='name' placeholder='product 1' className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
                 </div>
                 {/* category */}
                 <div>
                     <label className='text-sm font medium block text-gray-600'>category</label>
-                   <select  className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' name="categoy" id="category">
+                   <select  {...register("category", { required: true })}  className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' name="category" id="category">
                    <option value="">choose a  category</option>
                     <option value="Bags">Bags</option>
                     <option value="Shoe">Shoe</option>
@@ -23,20 +31,20 @@ const ADDproduct = () => {
                 {/* img url */}
                 <div>
                     <label className='text-sm font medium block text-gray-600'>image URL</label>
-                    <input type="text" name='image' id='image' placeholder='https//:' className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
+                    <input  {...register("image", { required: true })} type="text" name='image' id='image' placeholder='https//:' className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
                 </div>
                 {/* price */}
               <div className='flex items-center justify-center gap-4'>
               <div>
                     <label className='text-sm font medium block text-gray-600'>price</label>
-                    <input type="number" name='price' id='price'  className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
+                    <input  {...register("price", { required: true })} type="number" name='price' id='price' placeholder='100' className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
                 </div>
                 <div>
                     <label className='text-sm font medium block text-gray-600'>date</label>
-                    <input type="date" name='date' id='date'  className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
+                    <input  {...register("date", { required: true })} type="date" name='date' id='date'  className='w-full bg-gray-50 text-black mt-1 p-2 rounded-lg border focus:outline focus:ring-1 focus:ring-indigo-500' />
                 </div>
               </div>
-              <button type='submit btn-primary w-full'>Add Product</button>
+              <button type='submit' className=' btn btn-primary w-full'>Add Product</button>
             </form>
             
         </div>
