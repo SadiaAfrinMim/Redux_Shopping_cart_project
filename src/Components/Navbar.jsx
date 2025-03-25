5d608ace1bc3327177b879a4fd7712ebe7c5d8e5
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-25 22:55:40 +0600

    cart product increment

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router';
 
 const Navbar = () => {
+  const carts =useSelector((state)=>state.carts)
+  console.log(carts)
     return (
        <div className='bg-indigo-900 '>
          <div className="navbar max-w-7xl mx-auto shadow-sm">
