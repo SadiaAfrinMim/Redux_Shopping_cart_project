36cfbe06c156c880f81060b33d8a8090f7c1a179
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-25 23:59:29 +0600

    cart design ok and functionality done

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,14 @@ import Home from './Components/Pages/home/Home.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
     <Routes >
       <Route element={<App />}>
       <Route path="/" element={<Home></Home>}/>
-      <Route path="/cart" element={<div>product page</div>} />
+      <Route path="/cart" element={<CartPage></CartPage>} />
       <Route path="register" element={<div>register</div>} />
       
       </Route>
