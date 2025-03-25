af85d64bb0819d7b1152e3d9524e953c5502d8a0
SadiaAfrinMim <mimsadia367@gmail.com>
2025-03-25 22:36:15 +0600

    cartslice adding

--- src/redux/features/cart/cartSlice.jsx
+++ src/redux/features/cart/cartSlice.jsx
@@ -1,11 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
+
 const cartSlice = createSlice({
     name:'cart',
     initialState:[],
     reducers:{
         addToCart:(state,action)=>{
-            const existingProduct = state.find((product)=>product.id ===action.payload)
+            
+            const existingProduct = state.find((product)=>product.id===action.payload)
         }
     }
 })
+
+
+
+export const {addToCart} = cartSlice.actions;
+export default cartSlice.reducer 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productReducer from '../redux/features/product/productSlice'
+import cartReducer from '../redux/features/cart/cartSlice'
 
 const store = configureStore({
     reducer:{
-        products: productReducer
+        products: productReducer,
+        carts: cartReducer
     }
 })
 
